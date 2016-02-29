63e651fadfbd65bc76fd6afa44ae433cdc8b082a
pthreat <jpfstange@gmail.com>
2016-02-29 01:29:40 -0300

    Applying new pattern described in README

--- i.php
+++ i.php
@@ -1,10 +1,11 @@
 <?php
 
-	$file	=	'C:\Users\federico.stange\apf-dev\boot.php';
+	//$file	=	'C:\Users\federico.stange\apf-dev\boot.php';
 
-	require $file;
-	require 'test.php';
+//require $file;
 
-
-	$form = new TestForm();
-	$form->render();
+	require "/usr/share/apf-dev/boot.php";
+	
+	$ui	=	\apf\core\Project::configure();
+	$ui->render();
+	die();
